2d2ace13bc6824c79b436c1c7bf33e9e417070fd
Emircan Isanc <emircanisanc@gmail.com>
2023-08-25 09:22:21 +0300

    new shotgun

--- Dive Deeper/Assets/Scripts/FPS/Weapon/Shotgun.cs
+++ Dive Deeper/Assets/Scripts/FPS/Weapon/Shotgun.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Shotgun : GunBase
 {
     public int ammoPerShot = 3;
+    public Transform[] firepoints;
 
     public override void HandleSecondFire(Transform cam)
     {
@@ -41,7 +42,7 @@ public class Shotgun : GunBase
             for (int i = 0; i < ammoPerShot; i++)
             {
                 var newDir = dir + new Vector3(RandomDirMultiplier, RandomDirMultiplier, RandomDirMultiplier) * UnityEngine.Random.Range(-0.1f, 0.1f);
-                SendLine(newDir); // DEBUG LINE
+                SendLine(newDir, i); // DEBUG LINE
                 HandleHit(Physics.RaycastAll(startPos, newDir, range, targetLayer));
             }
             nextAttackTime = Time.timeSinceLevelLoad + fireRate;
@@ -50,26 +51,38 @@ public class Shotgun : GunBase
         return false;
     }
 
-    public float RandomDirMultiplier => Random.Range(0.4f, 0.6f) * RandomSign;
+    public float RandomDirMultiplier => Random.Range(0.3f, 0.4f) * RandomSign;
     public float RandomSign => Random.Range(0, 2) == 0 ? 1f : -1f;
 
-    protected override void SendLine(Vector3 dir)
+    protected void SendLine(Vector3 dir, int firePoint)
     {
         float min = UnityEngine.Random.Range(0.1f, 0.2f);
-        Vector3 start = firePoint.position + dir * min;
-        /* trailRenderer.transform.position = start;
-        trailRenderer.Clear(); // clear existing positions from the trail renderer
-        trailRenderer.enabled = true; // enable the trail renderer */
+        //Vector3 start = firePoint.position + dir * min;
+        Transform cam = Camera.main.transform;
+        Vector3 end =firepoints[firePoint].position + firepoints[firePoint].forward * 10;
+        var hits = Physics.RaycastAll(cam.position, firepoints[firePoint].forward, range, targetLayer);
+        if (hits.Length > 0)
+            end = hits[0].point;
 
+        Vector3 start = firepoints[firePoint].position + (end - firepoints[firePoint].position).normalized * 0.5f;
+
+        // StartCoroutine(DisableTrailAfterDelay(end)); // disable the trail renderer after a delay
+        TrailManager.Instance.CreateShotgunTrail(start, end);
+    }
+
+    /* protected override void SendLine(Vector3 dir)
+    {
+        float min = UnityEngine.Random.Range(0.1f, 0.2f);
+        //Vector3 start = firePoint.position + dir * min;
         Transform cam = Camera.main.transform;
         Vector3 end = cam.position + dir * 10;
         var hits = Physics.RaycastAll(cam.position, dir, range, targetLayer);
         if (hits.Length > 0)
             end = hits[0].point;
-        /* trailRenderer.AddPosition(start);
-        trailRenderer.AddPosition(end); */
+
+        Vector3 start = firePoint.position + (end - firePoint.position).normalized * 1f;
 
         // StartCoroutine(DisableTrailAfterDelay(end)); // disable the trail renderer after a delay
         TrailManager.Instance.CreateShotgunTrail(start, end);
-    }
+    } */
 }
