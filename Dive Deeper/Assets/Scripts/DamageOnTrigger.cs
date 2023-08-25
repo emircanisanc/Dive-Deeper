28a49ca6cc04dfd4936c7e1f92e155f0b32b2302
Yakup Ekinci <99822215+YakupEkinci@users.noreply.github.com>
2023-08-26 01:57:56 +0300

    level 2  lava OK

--- Dive Deeper/Assets/Scripts/DamageOnTrigger.cs
+++ Dive Deeper/Assets/Scripts/DamageOnTrigger.cs
@@ -7,9 +7,12 @@ public class DamageOnTrigger : MonoBehaviour
     public float damage = 50;
     void OnTriggerEnter(Collider other)
     {
+        //  Debug.Log("aaa");
         if (other.TryGetComponent<PlayerHealth>(out var damageable))
         {
+
+
             damageable.ApplyDamage(damage);
-        }    
+        }
     }
 }
