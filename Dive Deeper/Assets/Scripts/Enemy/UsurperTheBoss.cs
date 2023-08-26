910ec9c2aabec190c2b7278ed1044bb077f326a3
Emircan Isanc <emircanisanc@gmail.com>
2023-08-26 23:49:28 +0300

    opening scene

---
+++ Dive Deeper/Assets/Scripts/Enemy/UsurperTheBoss.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UsurperTheBoss : EnemyBaseAbstract
+{
+
+    
+
+
+    public override void Attack()
+    {
+        throw new System.NotImplementedException();
+    }
+
+
+
+    
+}
